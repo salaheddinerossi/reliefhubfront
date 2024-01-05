87234660b401340b8dcadc15412dfe16e5fcbadd
salaheddinerossi <salaheddine.rossi@etu.uae.ac.ma>
2024-01-05 23:53:26 +0100

    guards

--- src/app/core/authentication/authentication.guard.ts
+++ src/app/core/authentication/authentication.guard.ts
@@ -1,17 +1,44 @@
 import { inject } from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot} from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
 import { AuthenticationService } from './authentication.service';
 
-export const authGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+// Utility function to extract the role from the token
+function extractRoleFromToken(authService: AuthenticationService): string | null {
+  const currentUser = authService.currentUserValue;
+  if (!currentUser || !currentUser.token) {
+    return null;
+  }
+
+  try {
+    const tokenPayload = JSON.parse(atob(currentUser.token.split('.')[1]));
+    return tokenPayload.role;
+  } catch (error) {
+    return null;
+  }
+}
+
+export const organizationGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
   const authService = inject(AuthenticationService);
   const router = inject(Router);
-  const currentUser = authService.currentUserValue;
+  const role = extractRoleFromToken(authService);
 
-  if (currentUser) {
+  if (role === 'ROLE_ORGANIZATION') {
     return true;
   }
 
   router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
   return false;
+};
 
+export const adminGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+  const authService = inject(AuthenticationService);
+  const router = inject(Router);
+  const role = extractRoleFromToken(authService);
+
+  if (role === 'ROLE_ADMIN') {
+    return true;
+  }
+
+  router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+  return false;
 };
